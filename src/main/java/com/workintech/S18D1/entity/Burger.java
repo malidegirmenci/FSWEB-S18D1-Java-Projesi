331e2bcf023de1bab50bc904caec4df7288a4514
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-12-18 23:11:56 +0300

    changed schema name for postgresql

--- src/main/java/com/workintech/S18D1/entity/Burger.java
+++ src/main/java/com/workintech/S18D1/entity/Burger.java
@@ -7,22 +7,19 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @Entity
-@Table(name = "jpaburger", schema = "S18D1")
+@Table(name = "jpaburger", schema = "s18")
 public class Burger {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(name = "name")
     private String name;
     @Column(name = "price")
     private Integer price;
     @Column(name = "is_vegan")
     private Boolean isVegan;
-
     @Enumerated(EnumType.STRING)
     private BreadType breadType;
-
     @Column(name = "contents")
     private String contents;
 }
