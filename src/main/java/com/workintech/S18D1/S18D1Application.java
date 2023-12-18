4c1d4426c2e794c5f3b33d8f986df9cf67c5b9d2
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-12-18 22:53:19 +0300

    init spring boot

---
+++ src/main/java/com/workintech/S18D1/S18D1Application.java
@@ -0,0 +1,13 @@
+package com.workintech.S18D1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S18D1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S18D1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/workintech/S18D1/S18D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.S18D1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S18D1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
