9c4c65ea7dc6d5842757cf081dc6201b64cce6c2
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-12-19 03:10:21 +0300

    created exception handler

---
+++ src/main/java/com/workintech/S18D1/exception/BurgerErrorResponse.java
@@ -0,0 +1,10 @@
+package com.workintech.S18D1.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class BurgerErrorResponse {
+    private String message;
+}
---
+++ src/main/java/com/workintech/S18D1/exception/BurgerException.java
@@ -0,0 +1,13 @@
+package com.workintech.S18D1.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+@Getter
+public class BurgerException extends RuntimeException{
+    private HttpStatus httpStatus;
+
+    public BurgerException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+}
